c6ffb8a61c20d9b72a2e377afaca668520693dd4
Fredrik Lönnegren <fredrik.lonnegren@internetstiftelsen.se>
2020-04-15 12:07:32 +0200

    Apply st-alpha

--- config.def.h
+++ config.def.h
@@ -82,6 +82,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.99;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -109,6 +112,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#2e3440", /* background */
 	"#d8dee9", /* foreground */
+    "black",
 };
 
 
